b8a6f787b63f92cf461576b851d425c832fe5d71
Brayan <gonzalezbrayan2014@gmail.com>
2025-04-15 12:20:12 -0500

    added clear and change size buttons with working functions

--- javascript.js
+++ javascript.js
@@ -18,3 +18,28 @@ let draw = (event) => {
 }
 
 hover.addEventListener("mouseover", draw);
+
+let cbutton = document.querySelector(".clear");
+
+let clear = () => {
+    let children = hover.children;
+    for(let i = 0; i<children.length; i++){
+        children[i].style.backgroundColor = "black";
+    }
+}
+
+cbutton.addEventListener("click", clear);
+
+let csize = document.querySelector(".size");
+
+let changeSize = () => {
+    let size = prompt("Choose a size: ");
+    if(size>100){
+        alert("Size too big, max = 100!")
+    }else{
+        hover.innerHTML = "";
+        grid(size,size);
+    }
+}
+
+csize.addEventListener("click", changeSize);
